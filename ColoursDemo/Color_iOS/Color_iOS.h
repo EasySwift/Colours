f9470a3e294205de9d3c695686acdd7606ef9f03
stubbornnessness <1219721814yxj>
2016-10-31 23:55:41 +0800

    6.0.2

--- ColoursDemo/Color_iOS/Color_iOS.h
+++ ColoursDemo/Color_iOS/Color_iOS.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char Color_iOSVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Color_iOS/PublicHeader.h>
 
 
+#import "Colours.h"
